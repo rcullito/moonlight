ca6208003841f648a400628d70a116145cba8675
Rob Culliton <rob.culliton@gmail.com>
2020-07-02 11:41:02 -0400

    call start sensor function from the roll button. that activity seems to be starting up fine and is even recording the initial value for the azimuth

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -17,26 +19,11 @@ class MainActivity : AppCompatActivity() {
     Log.i("MainActivity", "onCreate Called yipee")
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    val rollButton: Button = findViewById(R.id.roll_button)
-    rollButton.setOnClickListener {
-      rollDice()
-    }
 
-    diceImage = findViewById(R.id.dice_image)
   }
 
-
-  private fun rollDice() {
-      Log.i("MainActivity", "rollDice called hooray")
-     val randomInt = Random().nextInt(6) + 1
-     val drawableResource = when (randomInt) {
-       1 -> R.drawable.dice_1
-       2 -> R.drawable.dice_2
-       3 -> R.drawable.dice_3
-       4 -> R.drawable.dice_4
-       5 -> R.drawable.dice_5
-       else -> R.drawable.dice_6
-     }
-    diceImage.setImageResource(drawableResource)
+  fun startSensor(view: View) {
+    val intent = Intent(this, SensorActivity::class.java)
+    startActivity(intent)
   }
 }
