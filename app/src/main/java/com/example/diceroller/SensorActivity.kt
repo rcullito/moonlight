5232217d05d3d0f8a05a224a7d065c6c755d8e09
Rob Culliton <rob.culliton@gmail.com>
2020-07-12 07:40:01 -0400

    empty sensorActivity of the outdate code. this will have to be completely redone. adjusting the tutorial from java to kotlin but at least it will compose the sensors in the correct way.

--- app/src/main/java/com/example/diceroller/SensorActivity.kt
+++ app/src/main/java/com/example/diceroller/SensorActivity.kt
@@ -12,53 +12,20 @@ import android.util.Log
 
 
 class SensorActivity : Activity(), SensorEventListener {
-  private var mSensorManager: SensorManager? = null
-  private var mOrientation: Sensor? = null
+
 
   public override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     Log.i("SensorActivity", "onCreate Called yipee")
-    mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    mOrientation = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+
   }
 
   override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    // Do something here if sensor accuracy changes.
-    // You must implement this callback in your code.
+    TODO("Not yet implemented")
   }
 
-  override fun onResume() {
-    super.onResume()
-    mSensorManager!!.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL)
+  override fun onSensorChanged(event: SensorEvent?) {
+    TODO("Not yet implemented")
   }
 
-  override fun onPause() {
-    super.onPause()
-    mSensorManager!!.unregisterListener(this)
-  }
-
-  override fun onSensorChanged(event: SensorEvent) {
-    val azimuth_angle = event.values[0]
-    val pitch_angle = event.values[1]
-    val roll_angle = event.values[2]
-    // Do something with these orientation angles.
-
-    // azimuth NORTH will be 0, SOUTH 180 EAST 90  WEST 270
-    // range is 0 to 360
-    // Log.i( "SensorActivity", azimuth_angle.toString())
-
-    // degree of rotation around the x axis, range 180 to -180
-    // hold your phone parallel to the floor with the bottom facing you.
-    // tilt the TOP towards the ground and the pitch becomes positive
-    // tilt the top up and the pitch becomes negative
-    // Log.i("SensorActivity", pitch_angle.toString())
-
-    // degree of rotation around the y axis. THIS IS MOST LIKELY WHAT WE WANT
-    // IF WE THINK ABOUT A DEVICE STRAPPED TO OUR ARM
-
-    // hold the device parallel to the ground with the bottom edge towards you
-    // tilt the LEFT EDGE to the ground for the roll angle to become positive
-    Log.i("SensorActivity", roll_angle.toString())
-
-  }
 }
