ed93f5b1f982d034e2937658342398182d3a8257
Rob Culliton <rob.culliton@gmail.com>
2020-07-12 08:09:39 -0400

    viewmodel added that does not break the build

---
+++ app/src/main/java/com/example/diceroller/LupinViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.diceroller
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LupinViewModel(application: Application) : AndroidViewModel(application) {
+
+}
