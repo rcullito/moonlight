d84c563b0a5817d5096526b239edce0c80663916
Rob Culliton <rob.culliton@gmail.com>
2020-07-12 07:31:20 -0400

    log simple message from worker and return Result.success() that seems to be the only requirement for a working implementation to get started

--- app/src/main/java/com/example/diceroller/workers/LogWorker.kt
+++ app/src/main/java/com/example/diceroller/workers/LogWorker.kt
@@ -1,11 +1,15 @@
 package com.example.diceroller.workers
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 class LogWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
   override fun doWork(): Result {
-    TODO("Not yet implemented")
+
+    Log.i("LogWorker", "Shut the door Alec, there's a draft")
+
+    return Result.success()
   }
 }
