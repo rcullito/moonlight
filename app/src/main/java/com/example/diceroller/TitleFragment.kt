80a50fb6634b489eb5b3d501726a439273736445
Rob Culliton <rob.culliton@gmail.com>
2020-07-22 08:20:38 -0400

    delete the sensor activity files that are no longer being used. continue to walk the path!

--- app/src/main/java/com/example/diceroller/TitleFragment.kt
+++ app/src/main/java/com/example/diceroller/TitleFragment.kt
@@ -1,11 +1,9 @@
 package com.example.diceroller
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
