c1cebac8f716a751162977f2a78edcb9123557a3
Rob Culliton <rob.culliton@gmail.com>
2020-07-24 08:16:21 -0400

    working drawer layout design. w00t

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.diceroller.databinding.ActivityMainBinding
@@ -14,19 +15,23 @@ import com.example.diceroller.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
+  private lateinit var drawerLayout: DrawerLayout
 
   override fun onCreate(savedInstanceState: Bundle?) {
+
     Log.i("MainActivity", "onCreate Called yipee")
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    drawerLayout = binding.drawerLayout
     val navController = this.findNavController(R.id.myNavHostFragment)
-    NavigationUI.setupActionBarWithNavController(this, navController)
 
+    NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    NavigationUI.setupWithNavController(binding.navView, navController)
 
   }
 
   override fun onSupportNavigateUp(): Boolean {
     val navController = this.findNavController(R.id.myNavHostFragment)
-    return navController.navigateUp()
+    return NavigationUI.navigateUp(navController, drawerLayout)
   }
 }
