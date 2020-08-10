e5a93b8c3912533d1afa2c92a1628069843539c0
Rob Culliton <rob.culliton@gmail.com>
2020-08-10 17:51:59 -0400

    ok, actually pretty easy to get the date from the bundle :) next step is to design a query that can take date as a string and get the proper results for a given day

--- app/src/main/java/com/example/diceroller/IndividualFragment.kt
+++ app/src/main/java/com/example/diceroller/IndividualFragment.kt
@@ -1,6 +1,7 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -33,27 +34,18 @@ class IndividualFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
+
+
+
+    val arguments = IndividualFragmentArgs.fromBundle(requireArguments())
+    val date = arguments.date
+
+    Log.i("IndividualFragment", date)
+
+
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_individual, container, false)
   }
 
-  companion object {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IndividualFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-      IndividualFragment().apply {
-        arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
-        }
-      }
-  }
+
 }
