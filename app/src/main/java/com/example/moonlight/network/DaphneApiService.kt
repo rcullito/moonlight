b2acb1c71a47f546aa9618af5435175364618805
Rob Culliton <rob.culliton@gmail.com>
2020-08-23 10:22:53 -0400

    something up with gradle connecting to the internet to pull down depenedncies

---
+++ app/src/main/java/com/example/moonlight/network/DaphneApiService.kt
@@ -0,0 +1,27 @@
+package com.example.moonlight.network
+
+/*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://mars.udacity.com/"
+
+private val retrofit = Retrofit.Builder()
+  .addConverterFactory(ScalarsConverterFactory.create())
+  .baseUrl(BASE_URL)
+  .build()
+
+interface DaphneApiService {
+  @GET("realestate")
+  fun getProperties(): Call<String>
+}
+
+object MarsApi {
+  val retrofitService: DaphneApiService by lazy {
+    retrofit.create(DaphneApiService::class.java)
+  }
+}
+
+ */
