a76494682841eb8a57249684739a159ecdaa0a18
Rob Culliton <rculliton@guaranteedrate.com>
2020-09-13 12:47:43 -0400

    rip out retrofit artifacts, though these are backed up on a branch just in case they are needed down the line

--- app/src/main/java/com/example/moonlight/dates/ResultsViewModel.kt
+++ app/src/main/java/com/example/moonlight/dates/ResultsViewModel.kt
@@ -1,13 +1,8 @@
 package com.example.moonlight.dates
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.example.moonlight.database.SleepPositionDao
-import com.example.moonlight.network.DaphneApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class ResultsViewModel(val database: SleepPositionDao, application: Application): AndroidViewModel(application) {
 
@@ -16,18 +11,5 @@ class ResultsViewModel(val database: SleepPositionDao, application: Application)
   }
 
   val dates = database.getUniqueDates()
-
-  fun getMarsRealEstateProperties() {
-    DaphneApi.retrofitService.getProperties().enqueue(object : Callback<String> {
-      override fun onFailure(call: Call<String>, t: Throwable) {
-        val marsError = "Failure: " + t.message
-        Log.i("Mars", marsError)
-      }
-
-      override fun onResponse(call: Call<String>, response: Response<String>) {
-        Log.i("Mars", response.body())
-      }
-    })
-  }
 
 }
