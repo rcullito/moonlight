cb122cfb243e3d14ad1d4d4726e297eb201f8855
Rob Culliton <rculliton@guaranteedrate.com>
2020-09-19 16:59:29 -0400

    rework with sturdier mapRange from rosetta code

--- app/src/main/java/com/example/moonlight/sensor/Angles.kt
+++ app/src/main/java/com/example/moonlight/sensor/Angles.kt
@@ -7,9 +7,18 @@ import androidx.annotation.RequiresApi
 import com.example.moonlight.*
 import com.example.moonlight.database.SleepPosition
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 val rotationMatrix = FloatArray(9)
 val orientationAngles = FloatArray(3)
+
+class FloatRange(override val start: Float, override val endInclusive: Float) : ClosedRange<Float>
+
+fun robRange(range1: FloatRange, range2: IntRange, value: Float): Int {
+  if (value !in range1) throw IllegalArgumentException("value is not within the first range")
+  if (range1.endInclusive == range1.start) throw IllegalArgumentException("first range cannot be single-valued")
+  return range2.start + ((value - range1.start) * (range2.endInclusive - range2.start) / (range1.endInclusive - range1.start)).toInt()
+}
 
 fun onBackAccordingToRoll(roll: Double): Boolean {
   val roll_absolute = abs(roll)
@@ -32,12 +41,6 @@ fun decideInRangePitch(pitch: Double, roll: Double): Boolean {
   }
 }
 
-// extension function assumed to always act on our time range
-fun ClosedFloatingPointRange<Double>.convert(number: Double, target: ClosedFloatingPointRange<Double>): Double {
-  val ratio = number.toFloat() / (endInclusive - start)
-  return ratio * (target.endInclusive - target.start)
-}
-
 fun buildMinuteRange(hoursList: List<Int>): MutableList<String> {
   var minutesHours = mutableListOf<String>()
   for (hour in hoursList) for (minute in 0..59)
@@ -49,17 +52,21 @@ fun buildMinuteRange(hoursList: List<Int>): MutableList<String> {
 val top_hours = listOf(9, 10, 11, 12, 1, 2)
 // get the count of this, and then the ratio will give us the index
 val top_of_clock_range = buildMinuteRange(top_hours)
+val top_of_clock_start_index = 0
+val top_of_clock_end_index = top_of_clock_range.count() - 1
+val clockIntRange = IntRange(top_of_clock_start_index, top_of_clock_end_index)
 
 
 
 
-fun cooCoo(pitch: Double, roll: Double): Double {
+fun cooCoo(pitch: Double, roll: Double): String {
   // if we are on our back according to roll, then our range is  9-12, 0-3
 
-
+  var floatPitch = pitch.toFloat()
   if (onBackAccordingToRoll(roll)) {
-    // TODO convert this to a const, pi and pi/2
-    val result =  (-1.57..1.57).convert(-0.78, top_of_clock_range)
+    var sixHourIndex =  robRange(FloatRange(-1.57f, 1.57f), clockIntRange, floatPitch)
+    var pitchClockTime = top_of_clock_range[sixHourIndex]
+    return pitchClockTime
   }
 
   // pitch of 0 is 12 noon
@@ -75,7 +82,7 @@ fun cooCoo(pitch: Double, roll: Double): Double {
   // then if want to get fancy, going from base 100 to base 60 for the minutes
   // round to nearest minute
 
-  return 12.57
+  return "up next is the other six hours"
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
