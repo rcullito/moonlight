ee0566b9c8df81d86fbe9b8df01f1187106b805a
Rob Culliton <rculliton@guaranteedrate.com>
2020-09-20 17:36:28 -0400

    scatter charts are simpler :)

--- app/src/main/java/com/example/moonlight/individual/NightChart.kt
+++ app/src/main/java/com/example/moonlight/individual/NightChart.kt
@@ -1,15 +1,19 @@
 package com.example.moonlight.individual
 
+import android.R
+import android.os.Bundle
 import com.example.moonlight.convertLongToTimeString
 import com.example.moonlight.database.SleepPosition
 import com.example.moonlight.databinding.FragmentIndividualBinding
 import com.example.moonlight.sensor.staticClockDataStructure
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.ScatterChart
 import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+
+
 
 class MyXAxisFormatter : ValueFormatter() {
   override fun getAxisLabel(value: Float, axis: AxisBase?): String {
@@ -24,7 +28,7 @@ class MyYAxisFormatter : ValueFormatter() {
 }
 
 fun buildChart(binding: FragmentIndividualBinding, sleepPositions: List<SleepPosition>) {
-  var chart: LineChart = binding.chart as LineChart
+  var chart: ScatterChart = binding.chart
   chart.isScaleYEnabled = false
 
   chart.axisRight.isEnabled = false
@@ -43,8 +47,8 @@ fun buildChart(binding: FragmentIndividualBinding, sleepPositions: List<SleepPos
     entries.add(Entry(position.sleepPositionTime.toFloat(), position.wallClock.toFloat()));
   }
 
-  var dataSet = LineDataSet(entries, "Sleep")
-  val lineData = LineData(dataSet)
-  chart.data = lineData
+  var dataSet = ScatterDataSet(entries, "Sleep")
+  val scatterData = ScatterData(dataSet)
+  chart.data = scatterData
   chart.data.isHighlightEnabled = false
 }
