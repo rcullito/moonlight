5b795716900530de16c07d2c30a0871cae3434e5
Rob Culliton <rculliton@guaranteedrate.com>
2020-09-20 14:43:15 -0400

    looks good, except we have two extra ints in the top half of the clock

--- app/src/main/java/com/example/moonlight/sensor/Angles.kt
+++ app/src/main/java/com/example/moonlight/sensor/Angles.kt
@@ -41,24 +41,6 @@ fun decideInRangePitch(pitch: Double, roll: Double): Boolean {
   }
 }
 
-fun buildMinuteRange(hoursList: List<Int>): MutableList<String> {
-  var minutesHours = mutableListOf<String>()
-  for (hour in hoursList) for (minute in 0..59)
-    minutesHours.add("$hour:$minute")
-
-  return minutesHours
-}
-
-val top_hours = listOf(9, 10, 11, 12, 1, 2)
-val bottom_hours = listOf(3, 4, 5, 6, 7, 8)
-
-val top_of_clock_range = buildMinuteRange(top_hours)
-val bottom_of_clock_range = buildMinuteRange(bottom_hours)
-
-val top_of_clock_start_index = 0
-val top_of_clock_end_index = top_of_clock_range.count() - 1
-val clockIntRange = IntRange(top_of_clock_start_index, top_of_clock_end_index)
-
 fun cooCoo(pitch: Double, roll: Double): String {
 
   var floatPitch = pitch.toFloat()
---
+++ app/src/main/java/com/example/moonlight/sensor/buckBeak.kt
@@ -0,0 +1,33 @@
+package com.example.moonlight.sensor
+
+fun buildMinuteRange(hoursList: List<Int>): MutableList<String> {
+  var minutesHours = mutableListOf<String>()
+  for (hour in hoursList) for (minute in 0..59)
+    minutesHours.add("$hour:$minute")
+
+  return minutesHours
+}
+
+val top_hours = listOf(9, 10, 11, 12, 1, 2)
+val bottom_hours = listOf(3, 4, 5, 6, 7, 8)
+
+val top_of_clock_range = buildMinuteRange(top_hours)
+val bottom_of_clock_range = buildMinuteRange(bottom_hours)
+
+val top_of_clock_start_index = 0
+val top_of_clock_end_index = top_of_clock_range.count() - 1
+val clockIntRange = IntRange(top_of_clock_start_index, top_of_clock_end_index)
+
+
+// data structure for charting lib
+fun buildTopHalfOfClockInts(): MutableList<Int> {
+  var topOfClockInts = mutableListOf<Int>()
+  for (i in (540..720)) topOfClockInts.add(i)
+  for (i in (0..180)) topOfClockInts.add(i)
+  return topOfClockInts
+}
+
+
+
+
+
