a3246df9783be67740612978832192587789f1e4
Rob Culliton <rob.culliton@gmail.com>
2020-10-10 17:27:56 -0400

    order by timestamp DESC to get the most recent dates first

--- app/src/main/java/com/example/moonlight/database/SleepPositionDao.kt
+++ app/src/main/java/com/example/moonlight/database/SleepPositionDao.kt
@@ -17,7 +17,7 @@ interface SleepPositionDao {
   @Query("SELECT * FROM sleep_position_table ORDER BY sleepPositionId DESC LIMIT 1")
   fun getMostRecentPosition(): SleepPosition?
 
-  @Query("SELECT DISTINCT date(round(sleep_position_timestamp / 1000), 'unixepoch') AS date FROM sleep_position_table")
+  @Query("SELECT DISTINCT date(round(sleep_position_timestamp / 1000), 'unixepoch') AS date FROM sleep_position_table ORDER BY sleep_position_timestamp DESC")
   fun getUniqueDates(): LiveData<List<SleepDate>>
 
   @Query("SELECT * FROM sleep_position_table WHERE date(round(sleep_position_timestamp / 1000), 'unixepoch') BETWEEN :beforeDate AND :afterDate")
