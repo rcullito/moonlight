87a21fab15459e304e029b6332de87deb71daff7
Rob Culliton <rob.culliton@gmail.com>
2020-11-03 09:53:23 -0500

    clean up imports in util

--- app/src/main/java/com/example/moonlight/Util.kt
+++ app/src/main/java/com/example/moonlight/Util.kt
@@ -1,14 +1,6 @@
 package com.example.moonlight
 
-import android.os.Build
-import android.text.Html
-import android.text.Spanned
-import android.util.Log
-import androidx.core.text.HtmlCompat
-import com.example.moonlight.database.SleepPosition
-import com.example.moonlight.sensor.onBackAccordingToRoll
 import java.text.SimpleDateFormat
-import kotlin.math.abs
 
 
 fun convertLongToTimeString(systemTime: Long, desiredDateFormat: String): String {
