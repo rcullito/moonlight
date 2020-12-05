68d2b1e79358376bb9c3c739caeda00190f7e548
Rob Culliton <rob.culliton@gmail.com>
2020-12-05 12:56:50 -0500

    be bold about condensing settings down onto the record screen

--- app/src/main/java/com/example/moonlight/sensor/SensorFragment.kt
+++ app/src/main/java/com/example/moonlight/sensor/SensorFragment.kt
@@ -23,6 +23,23 @@ fun <T> Context.isServiceRunning(service: Class<T>): Boolean {
     .any { it -> it.service.className == service.name }
 }
 
+fun checkedIdToInterfere(checkedId: Int): String {
+  return when (checkedId) {
+    R.id.radio_button_1 -> "vibrate"
+    R.id.radio_button_2 -> "chime"
+    R.id.radio_button_3 -> "both"
+    else -> "none"
+  }
+}
+
+fun checkedToBound(checkedId: Int): String {
+  return when (checkedId) {
+    R.id.radio_button_5 -> "10-2"
+    R.id.radio_button_6 -> "9-3"
+    else -> "8-4"
+  }
+}
+
 class SensorFragment : Fragment() {
 
   private lateinit var binding: FragmentSensorBinding
@@ -35,9 +52,11 @@ class SensorFragment : Fragment() {
     if (running!!) {
       binding.cancelServiceButton.visibility = View.VISIBLE
       binding.startServiceButton.visibility = View.INVISIBLE
+      binding.radioGroup1.isEnabled = false
     } else {
       binding.cancelServiceButton.visibility = View.INVISIBLE
       binding.startServiceButton.visibility = View.VISIBLE
+      binding.radioGroup1.isEnabled = true
     }
   }
 
@@ -51,8 +70,29 @@ class SensorFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
 
+    val sharedPref = activity?.getSharedPreferences(
+      getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
     binding = DataBindingUtil.inflate<FragmentSensorBinding>(inflater,
       R.layout.fragment_sensor, container, false)
+
+    binding.setLifecycleOwner(this)
+
+    val interfere = sharedPref?.getString("interfere", "none")
+    val boundary = sharedPref?.getString("boundary", "8-4")
+
+    when (interfere) {
+      "vibrate" -> binding.radioButton1.isChecked = true
+      "chime" -> binding.radioButton2.isChecked = true
+      "both" -> binding.radioButton3.isChecked = true
+      else -> binding.radioButton4.isChecked = true
+    }
+
+    when (boundary) {
+      "10-2" -> binding.radioButton5.isChecked = true
+      "9-3" -> binding.radioButton6.isChecked = true
+      else -> binding.radioButton7.isChecked = true
+    }
 
     var running = isSensorServiceRunning()
     updateButtons(running, binding)
@@ -82,6 +122,28 @@ class SensorFragment : Fragment() {
 
       var running = isSensorServiceRunning()
       updateButtons(running, binding)
+    }
+
+    binding.radioGroup1.setOnCheckedChangeListener { _, checkedId ->
+
+      var interfereChoice = checkedIdToInterfere(checkedId)
+
+      with (sharedPref?.edit()) {
+        this?.putString("interfere", interfereChoice)
+        this?.apply()
+      }
+
+    }
+
+    binding.radioGroup2.setOnCheckedChangeListener { _, checkedId ->
+
+      with (sharedPref?.edit()) {
+
+        var boundChoice = checkedToBound(checkedId)
+
+        this?.putString("boundary", boundChoice)
+        this?.apply()
+      }
     }
     // Inflate the layout for this fragment
     return binding.root
