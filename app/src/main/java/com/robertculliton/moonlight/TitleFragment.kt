54ccc97017f3d02a0599a15ebc6ce070e45689c2
Rob Culliton <rob.culliton@gmail.com>
2021-01-09 12:07:43 -0500

    working hyperlink. still need to add layout constraints, proper styling, and the correct target :)

--- app/src/main/java/com/robertculliton/moonlight/TitleFragment.kt
+++ app/src/main/java/com/robertculliton/moonlight/TitleFragment.kt
@@ -1,7 +1,10 @@
 package com.robertculliton.moonlight
 
 import android.os.Bundle
-import android.view.*
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.robertculliton.moonlight.databinding.FragmentTitleBinding
@@ -16,6 +19,11 @@ class TitleFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
     val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+
+
+    var textView = binding.textViewLink
+    textView.setMovementMethod(LinkMovementMethod.getInstance());
 
     setHasOptionsMenu(true)
     return binding.root
